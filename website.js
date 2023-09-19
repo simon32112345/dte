e9cd9ca4c31a2e95858e05cda88d7f427d7f5b2c
simon32112345 <46249510+simon32112345@users.noreply.github.com>
2023-09-19 11:15:22 +0200

    Create website.js

---
+++ website.js
@@ -0,0 +1,9 @@
+var password = "6250";
+	function passcheck() {
+	if(document.getelementbyid('pass1').value != password {
+		alert('Wrong Password');
+	return false;
+	}
+	if(document.getelementbyid('pass1').value == password {
+		alert('Correct Password Click OK to enter');
+	}}
